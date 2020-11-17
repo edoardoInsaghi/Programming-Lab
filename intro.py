7137f4b62a5a0ca6817e47b3e4abc83f4aac2d66
repl.it user <replituser@example.com>
2020-11-17 15:31:08 +0000

    commit shampoo

---
+++ intro.py
@@ -0,0 +1,19 @@
+f=open("shampoo_sales.csv", "r")
+total_values=[]
+sum=0
+
+for line in f:
+    elements=line.split(",")
+
+    if elements[0] != "Date":
+        date=elements[0]
+        value=elements[1]
+
+        sum+=float(value)
+
+        total_values.append(float(value))
+
+print(total_values)
+print(sum)        
+
+
